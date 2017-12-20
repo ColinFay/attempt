22958198545844b9b488c5d4c98eece2344390c4
Colin <colin@thinkr.fr>
2017-12-20 17:29:02 +0100

    new function if_else

--- R/if.R
+++ R/if.R
@@ -21,6 +21,37 @@ if_then <- function(.x, .p = isTRUE, .f) {
   }
 }
 
+#' If this, then do that
+#'
+#' @param .x the object to test
+#' @param .p the predicate for testing. Defaut is \code{isTRUE}
+#' @param .f what to do if TRUE
+#' @param .else what to do if TRUE
+#'
+#' @return the evalution
+#' @export
+#'
+#' @examples
+#' a <- if_else(1, is.numeric, ~ return("Yay"), ~ return("Nay"))
+
+if_else <- function(.x, .p = isTRUE, .f, .else) {
+  if (is_formula(.f)){
+    .f <- as_mapper(.f)
+  } else {
+    .f <- enexpr(.f)
+  }
+  if (is_formula(.else)){
+    .else <- as_mapper(.else)
+  } else {
+    .else <- enexpr(.else)
+  }
+  if ( as_mapper(.p)(.x) ){
+      .f()
+  } else {
+    .else()
+  }
+}
+
 #' Test for all, any or none
 #'
 #' @param .l the list to test
