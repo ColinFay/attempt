a2477bfa71caa679cbc1688c261c82b8baf509a9
colin <colin@thinkr.fr>
2024-08-23 12:45:09 +0200

    doc: package doc

--- R/attempt-package.R
+++ R/attempt-package.R
@@ -1,13 +1,6 @@
-#' attempt package
-#'
-#' Tools for defensive programming in R.
-#' 'attempt' extends and facilitates defensive programming by providing a consistent grammar, and
-#' provides a set of easy to use functions for common tests and conditions. 'attempt' only depends
-#' on \code{rlang} & \code{withr}, and focuses on speed, so it can be easily integrated in other functions and used
-#' in data analysis.
-#'
-#' @name attempt-package
-#' @aliases attempt-package
-#' @docType package
-#' @author colin <contact@@colinfay.me>
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
 NULL
